12705dce315c82a46e0a97cf218ce3f6e5a27d07
Oliver Reinhard <oliver.reinhard@gmail.com>
2019-02-03 17:27:25 +0100

    Removed Sms language (will be replaced with Sim language)

--- com.mimacom.ddd.sm.sms/src/com/mimacom/ddd/sm/sms/impl/SPrimitiveImplCustom.java
+++
@@ -1,9 +0,0 @@
-package com.mimacom.ddd.sm.sms.impl;
-
-public class SPrimitiveImplCustom extends SPrimitiveImpl {
-	
-	@Override
-	public boolean isArchetype() {
-		return redefines == null;
-	}
-}
