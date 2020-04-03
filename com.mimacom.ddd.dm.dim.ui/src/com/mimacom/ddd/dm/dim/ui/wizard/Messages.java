6fa2c5779029b25c3bdd87c067864b063cb1529b
Oliver Reinhard <oliver.reinhard@mimacom.com>
2020-04-03 22:06:22 +0200

    Make minor enhancements in DIM, PUB

---
+++ com.mimacom.ddd.dm.dim.ui/src/com/mimacom/ddd/dm/dim/ui/wizard/Messages.java
@@ -0,0 +1,18 @@
+package com.mimacom.ddd.dm.dim.ui.wizard;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "com.mimacom.ddd.dm.dim.ui.wizard.messages"; //$NON-NLS-1$
+	
+	public static String HelloWorldFile_Label;
+	public static String HelloWorldFile_Description;
+	
+	static {
+	// initialize resource bundle
+	NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
